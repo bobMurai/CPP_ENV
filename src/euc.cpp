9cdcc44804e14423ac3d14f85e67e7641d6f227a
bobMurai <murai.sho@gmail.com>
2021-03-21 22:08:07 +0900

    微調整

--- src/euc.cpp
+++ src/euc.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int main()
-{
-    int a, b;
-    cin >> a >> b;
-    cout << GCD(a, b) << endl;
-}
-
 int GCD(int m, int n)
 {
     if (n == 0)
         return m;
     return GCD(n, m % n);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << GCD(a, b) << endl;
 }
