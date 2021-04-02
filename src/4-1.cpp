9065acde16f742f0fd432b1836964453e03d4be6
Sho MURAI <sho.murai@ibm.com>
2021-04-02 22:29:48 +0900

    DP化で計算量削減

--- src/4-1.cpp
+++ src/4-1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+vector<long long> memo;
 
 int tribo(int n)
 {
@@ -12,12 +15,18 @@ int tribo(int n)
     {
         return 1;
     }
-    return tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
+
+    if(memo[n] == -1){
+        memo[n] = tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
+    }
+
+    return memo[n];
 }
 
 int main()
 {
     int n;
     cin >> n;
+    memo.assign(n, -1);
     cout << tribo(n) << endl;
 }
